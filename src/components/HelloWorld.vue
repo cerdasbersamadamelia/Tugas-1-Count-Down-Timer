a67cfa7ba0dbce585482a55d1255798e22c89c68
unknown <pandaidancepat@gmail.com>
2022-11-25 11:48:17 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>Halo Damelia [041643527], detik berjalan mundur dari:  {{ countDown }} </p>
+</template>
+
+<script>
+   export default {
+       data () {
+           return {
+               countDown: 30
+           }
+       },
+       methods: {
+           countDownTimer () {
+               if (this.countDown > 0) {
+                   setTimeout(() => {
+                       this.countDown -= 1
+                       this.countDownTimer()
+                   }, 1000)
+               }
+           }
+       },
+       created () {
+           this.countDownTimer()
+       }
+   }
+</script>
